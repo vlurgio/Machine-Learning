cb2c4b9f65cd17c3179d4d2a8718cdbca5dc9291
Vinny Lurgio <13vlurgio@gmail.com>
2017-12-29 01:29:15 -0500

    Finished section 1 of course READY TO START MACHINE LEARNING!

---
+++ Machine_Learning_A-Z_Template_Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/DataPreprocessingTemplatePractice.R
@@ -0,0 +1,30 @@
+# Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding catagorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+traing_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+traing_set[, 2:3] = scale(traing_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
