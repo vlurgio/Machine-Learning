dbffe4776ad9fd6bce869896c2b8a34d5ded6dad
Vinny Lurgio <13vlurgio@gmail.com>
2018-01-03 18:18:25 -0500

    Finished Linear Regression in Python

--- Machine_Learning_A-Z_Template_Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine_Learning_A-Z_Template_Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -14,6 +14,7 @@ y = dataset.iloc[:, 1].values
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
+
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -21,3 +22,31 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set Results
+y_pred = regressor.predict(X_test)
+
+# Visualizing the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = "blue")
+plt.title("Salary vs Experience (Train)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
+
+# Visualizing the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = "blue")
+plt.title("Salary vs Experience (Test)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
+
+
+
+
