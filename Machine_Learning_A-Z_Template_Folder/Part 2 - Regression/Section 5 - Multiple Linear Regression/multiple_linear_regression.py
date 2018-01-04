0966d69b04d68fd4e6915f593a78c2fdf9bcb9c4
Vinny Lurgio <13vlurgio@gmail.com>
2018-01-03 19:32:39 -0500

    update

--- Machine_Learning_A-Z_Template_Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine_Learning_A-Z_Template_Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -1,4 +1,4 @@
-# Multiple Linear Regression
+# Data Preprocessing Template
 
 # Importing the libraries
 import numpy as np
@@ -6,19 +6,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
+dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 4].values
-
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
-X = onehotencoder.fit_transform(X).toarray()
-
-# Avoiding the Dummy Variable Trap
-X = X[:, 1:]
+y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
@@ -31,11 +21,3 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
-
-# Fitting Multiple Linear Regression to the Training set
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
