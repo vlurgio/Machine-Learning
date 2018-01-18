f156b31dbbb9ead53421bfcf384e39b251d2fb88
Vinny Lurgio <13vlurgio@gmail.com>
2018-01-18 16:43:23 -0500

    Finished part 4 - Clustering

--- Machine_Learning_A-Z_Template_Folder/Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.R
+++ Machine_Learning_A-Z_Template_Folder/Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.R
@@ -19,14 +19,11 @@ test_set = subset(dataset, split == FALSE)
 training_set[-3] = scale(training_set[-3])
 test_set[-3] = scale(test_set[-3])
 
-# Fitting Random Forest Classification to the Training set
-# install.packages('randomForest')
+# Fitting classifier to the Training set
 library(randomForest)
-set.seed(123)
 classifier = randomForest(x = training_set[-3],
                           y = training_set$Purchased,
-                          ntree = 500)
-
+                          ntree = 10)
 # Predicting the Test set results
 y_pred = predict(classifier, newdata = test_set[-3])
 
@@ -36,13 +33,13 @@ cm = table(test_set[, 3], y_pred)
 # Visualising the Training set results
 library(ElemStatLearn)
 set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.001)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.001)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, grid_set)
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Random Forest Classification (Training set)',
+     main = 'Classifier (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -56,13 +53,10 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, grid_set)
-plot(set[, -3], main = 'Random Forest Classification (Test set)',
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3], main = 'Classifier (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
-
-# Choosing the number of trees
-plot(classifier)
--- Machine_Learning_A-Z_Template_Folder/Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.R
+++ Machine_Learning_A-Z_Template_Folder/Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.R
@@ -2,46 +2,28 @@
 
 # Importing the dataset
 dataset = read.csv('Mall_Customers.csv')
-dataset = dataset[4:5]
+X = dataset[4:5]
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-# library(caTools)
-# set.seed(123)
-# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
-
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
-# Using the elbow method to find the optimal number of clusters
+# Using the elbow method to find the optimal number of clusters 
 set.seed(6)
-wcss = vector()
-for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
-plot(1:10,
-     wcss,
-     type = 'b',
-     main = paste('The Elbow Method'),
-     xlab = 'Number of clusters',
-     ylab = 'WCSS')
+wcss <- vector()
+for (i in 1:10) wcss[i] = sum(kmeans(X, i)$withinss)
+plot(1:10, wcss, type = 'b', main = paste('Clusters of clients'), xlab = 'Number of Clusters', ylab = 'WCSS')
 
-# Fitting K-Means to the dataset
+# Applying k-means to the mall dataset
 set.seed(29)
-kmeans = kmeans(x = dataset, centers = 5)
-y_kmeans = kmeans$cluster
+kmeans = kmeans(X, 5, iter.max = 300, nstart = 10)
 
-# Visualising the clusters
+# Visualizing the clusters
 library(cluster)
-clusplot(dataset,
-         y_kmeans,
+clusplot(X,
+         kmeans$cluster,
          lines = 0,
          shade = TRUE,
-         color = TRUE,
-         labels = 2,
+         clolor = TRUE,
+         labels =  2,
          plotchar = FALSE,
          span = TRUE,
-         main = paste('Clusters of customers'),
-         xlab = 'Annual Income',
-         ylab = 'Spending Score')
+         main = paste('Clusters of clients'),
+         xlab = 'Annual income',
+         ylab = 'Spending Score (1 - 100)')
