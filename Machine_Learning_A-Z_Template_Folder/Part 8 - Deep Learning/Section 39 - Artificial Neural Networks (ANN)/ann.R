3205da72d4cd59e7e4b9e7b079f9895813fc4297
Vinny Lurgio <13vlurgio@gmail.com>
2018-01-31 21:08:55 -0500

    Finish Artificial neural networks in R

--- Machine_Learning_A-Z_Template_Folder/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.R
+++ Machine_Learning_A-Z_Template_Folder/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.R
@@ -5,12 +5,15 @@ dataset = read.csv('Churn_Modelling.csv')
 dataset = dataset[4:14]
 
 # Encoding the categorical variables as factors
+# Encoding the target feature as factor
 dataset$Geography = as.numeric(factor(dataset$Geography,
                                       levels = c('France', 'Spain', 'Germany'),
                                       labels = c(1, 2, 3)))
 dataset$Gender = as.numeric(factor(dataset$Gender,
-                                   levels = c('Female', 'Male'),
-                                   labels = c(1, 2)))
+                                   levels = c('Male', 'Female'),
+                                   labels = c(2, 1)))
+
+
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
@@ -24,23 +27,21 @@ test_set = subset(dataset, split == FALSE)
 training_set[-11] = scale(training_set[-11])
 test_set[-11] = scale(test_set[-11])
 
-# Fitting ANN to the Training set
+# Fitting the ANN
 # install.packages('h2o')
 library(h2o)
 h2o.init(nthreads = -1)
-model = h2o.deeplearning(y = 'Exited',
-                         training_frame = as.h2o(training_set),
-                         activation = 'Rectifier',
-                         hidden = c(5,5),
-                         epochs = 100,
-                         train_samples_per_iteration = -2)
+classifer = h2o.deeplearning(y = 'Exited',
+                             training_frame = as.h2o(training_set),
+                             activation = 'Rectifier',
+                             hidden = c(6, 6),
+                             epochs = 100,
+                             train_samples_per_iteration = -2)
 
 # Predicting the Test set results
-y_pred = h2o.predict(model, newdata = as.h2o(test_set[-11]))
-y_pred = (y_pred > 0.5)
+prob_pred = h2o.predict(classifer, newdata = as.h2o(test_set[-11]))
+y_pred = ifelse(prob_pred > 0.5, 1, 0)
 y_pred = as.vector(y_pred)
 
 # Making the Confusion Matrix
 cm = table(test_set[, 11], y_pred)
-
-# h2o.shutdown()
